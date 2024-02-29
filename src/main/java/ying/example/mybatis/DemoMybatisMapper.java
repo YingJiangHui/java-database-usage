fabc053289ddf7436e7d8ebcb7744e37013b7028
Jh <473380917@qq.com>
2024-02-29 14:45:53 +0800

    使用Mapper方式访问数据库

---
+++ src/main/java/ying/example/mybatis/DemoMybatisMapper.java
@@ -0,0 +1,23 @@
+package ying.example.mybatis;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class DemoMybatisMapper {
+    public static void main(String[] args) throws IOException {
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession session =sqlSessionFactory.openSession();
+        StudentMapper studentMapper = session.getMapper(StudentMapper.class);
+        Student student = studentMapper.selectStudent(1);
+        System.out.println(student);
+
+    }
+}
