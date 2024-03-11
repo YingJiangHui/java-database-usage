4625bdc263f495506a48da6a70f2cc2e442097ac
Jh <473380917@qq.com>
2024-03-11 16:52:56 +0800

    使用mybatis generator逆向工程自动生成数据库操作语句

---
+++ src/main/java/ying/example/mybatis/mapper/StudentPOMapper.java
@@ -0,0 +1,30 @@
+package ying.example.mybatis.mapper;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+import ying.example.mybatis.pojo.StudentPO;
+import ying.example.mybatis.pojo.StudentPOExample;
+
+public interface StudentPOMapper {
+    long countByExample(StudentPOExample example);
+
+    int deleteByExample(StudentPOExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(StudentPO row);
+
+    int insertSelective(StudentPO row);
+
+    List<StudentPO> selectByExample(StudentPOExample example);
+
+    StudentPO selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("row") StudentPO row, @Param("example") StudentPOExample example);
+
+    int updateByExample(@Param("row") StudentPO row, @Param("example") StudentPOExample example);
+
+    int updateByPrimaryKeySelective(StudentPO row);
+
+    int updateByPrimaryKey(StudentPO row);
+}
---
+++ src/main/java/ying/example/mybatis/pojo/StudentPO.java
@@ -0,0 +1,53 @@
+package ying.example.mybatis.pojo;
+
+public class StudentPO {
+    private Integer id;
+
+    private String name;
+
+    private Integer age;
+
+    private String country;
+
+    private String className;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+}
