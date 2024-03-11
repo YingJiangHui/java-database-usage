0ddec7719f517ffbce7948a59417dfc00460c56e
Jh <473380917@qq.com>
2024-03-11 15:25:42 +0800

    将数据库字段映射到POJO类

--- src/main/java/ying/example/mybatis/Student.java
+++ src/main/java/ying/example/mybatis/Student.java
@@ -1,16 +1,14 @@
 package ying.example.mybatis;
 
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
+
 public class Student {
+    int id;
     String name;
     int age;
+    String className;
 
-    @Override
-    public String toString() {
-        return "Student{" +
-                "name='" + name + '\'' +
-                ", age=" + age +
-                '}';
-    }
 
     public void setAge(int age) {
         this.age = age;
@@ -26,5 +24,18 @@ public class Student {
 
     public int getAge() {
         return age;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", className='" + className + '\'' +
+                '}';
     }
 }
--- src/main/java/ying/example/mybatis/StudentMapper.java
+++ src/main/java/ying/example/mybatis/StudentMapper.java
@@ -1,23 +1,31 @@
 package ying.example.mybatis;
 
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
 
 import java.util.HashMap;
 
 public interface StudentMapper {
     public Student selectStudent(int id);
+
     public Student[] selectStudentByNameIfPresent(String name);
 
     public Student[] selectStudentByNameIfPresentV2(String name);
 
     public Student[] selectStudentByNameAndAge(String name, int age);
 
-    public Student[] selectStudentByNameAndAgeV2(@Param("name") String name,@Param("age") int age);
+    public Student[] selectStudentByNameAndAgeV2(@Param("name") String name, @Param("age") int age);
 
     public Student[] selectByObject(Student student);
 
+//    @Results(
+//            {@Result(column = "name", property = "name"),
+//                    @Result(column = "age", property = "age"),
+//                    @Result(column = "class_name", property = "className")}
+//    )
     public Student[] selectByObjectV2(@Param("student") Student student);
 
-    public Student[] selectByMap(@Param("map") HashMap<String,Object> map);
+    public Student[] selectByMap(@Param("map") HashMap<String, Object> map);
 
 }
